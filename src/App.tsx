2e994a2e71cb7152556c05774a9bcbc7721eeb0c
Texterly <88582978+Texterly@users.noreply.github.com>
2023-01-09 20:53:51 +0300

    added Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import FavouritesPage from './pages/FavouritesPage';
+import HomePage from './pages/HomePage';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <h1 className='font-bold'>Hello</h1>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path='/' element={ <HomePage /> } />
+        <Route path='/favourites' element={ <FavouritesPage /> } />
+      </Routes>
+    </>
   );
 }
 
