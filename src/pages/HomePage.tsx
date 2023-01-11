35201bc7a97decc76ee1c265ded604764519ebd1
Texterly <88582978+Texterly@users.noreply.github.com>
2023-01-11 22:05:31 +0300

    Set up search

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDebounce } from '../hooks/debounce'
 import { useSearchUsersQuery } from '../store/github/github.api'
 
 function HomePage() {
-  const {isLoading, isError, data} = useSearchUsersQuery('vladilen')
   const [search, setSearch] = useState('')
+  const [dropdown, setDropdown] = useState(false)
+  const debounced = useDebounce(search)
+  const {isLoading, isError, data} = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3
+  })
 
-  console.log(data);
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && data?.length! > 0)
+  }, [debounced, data])
   
   return (
     <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
@@ -19,9 +26,15 @@ function HomePage() {
           value={search}
           onChange = {e => setSearch(e.target.value)}
         />
-        <div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, in?
-        </div>
+        { dropdown && <ul className='list-none absolute top-[42px] left-0 right-0 max-h-[200px] overflow-y-scroll shadow-md bg-white'>
+          { isLoading && <p className='text-center'>Loading...</p> }
+          { data?.map(user=>  (
+            <li
+              key={user.id}
+              className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer'
+            >{ user.login }</li>
+          )) }
+        </ul> }
       </div>
     </div>
   )
